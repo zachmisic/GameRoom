6bc94b583655544ac34da926c332223eaf16fb0d
b185j406 <78661382+b185j406@users.noreply.github.com>
2021-03-22 16:43:11 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,25 +5,37 @@ int main()
 {
 	int option;
 	
-	std::cout<<"Main Menu\n\n1. Hangman\n2. Memory\n3. Snake\n4. Blackjack\n5. Checkers\n\n";
-	std::cout<<"Which game would you like to play?: ";
-	std::cin>>option;
-	if(option == 1)
+	do
 	{
-		Hangman HMprogram;
-		HMprogram.run();
-	}
-	if(option == 2)
-	{
-	}
-	if(option == 3)
-	{
-	}
-	if(option == 4)
-	{
-	}
-	if(option == 5)
-	{
-	}
+		std::cout<<"\nMain Menu\n\n1. Hangman\n2. Memory\n3. Snake\n4. Blackjack\n5. Checkers\n6. Quit\n\n";
+		std::cout<<"Select an option: ";
+		std::cin>>option;
+		if(option == 1)
+		{
+			Hangman HMprogram;
+			HMprogram.run();
+		}
+		else if(option == 2)
+		{
+		}
+		else if(option == 3)
+		{
+		}
+		else if(option == 4)
+		{
+		}
+		else if(option == 5)
+		{
+		}
+		else if(option == 6)
+		{
+			std::cout<<"exiting...\n";
+		}
+		else
+		{
+			std::cout<<"Invaid option\n";
+		}
+	}while(option != 6);
+	
 return(0);
 }
