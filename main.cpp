d5d5449d18a97dd95b49e9fcd3a131e088016870
b185j406 <78661382+b185j406@users.noreply.github.com>
2021-03-22 16:29:56 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,27 @@
 
 int main()
 {
-	Hangman program;
-	program.run();
+	int option;
+	
+	std::cout<<"Main Menu\n\n1. Hangman\n2. Memory\n3. Snake\n4. Blackjack\n5. Checkers\n\n";
+	std::cout<<"Which game would you like to play?: ";
+	std::cin>>option;
+	if(option == 1)
+	{
+		Hangman HMprogram;
+		HMprogram.run();
+	}
+	if(option == 2)
+	{
+	}
+	if(option == 3)
+	{
+	}
+	if(option == 4)
+	{
+	}
+	if(option == 5)
+	{
+	}
 return(0);
 }
