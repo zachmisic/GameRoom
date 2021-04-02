46b76d6c48d307801c9aa4937d5ef27e70573026
b185j406 <78661382+b185j406@users.noreply.github.com>
2021-04-02 09:49:25 -0500

    Update Hangman.h

--- Hangman.h
+++ Hangman.h
@@ -22,60 +22,60 @@ class Hangman
 		int guessesLeft = 6;
 		char playAgain;
 	public:
-		Hangman();
 		/**
 		* @pre The class is created
 		* @post The class is created
 		* @param none
 		* @return N/A
 		**/
+		Hangman();
 	
-		void run();
 		/**
 		* @pre we need the game to run
 		* @post game is run, communicates with all other classes
 		* @param none
 		* @return N/A
 		**/
+		void run();
 	
-		void wordGenerator();
 		/**
 		* @pre need to convert word to dashed string
 		* @post dashed string is created
 		* @param none
 		* @return N/A
 		**/
+		void wordGenerator();
 	
-		void display();
 		/**
 		* @pre need to display the hangman board
 		* @post hangman board is generated
 		* @param none
 		* @return N/A
 		**/
+		void display();
 	
-		void personDisplay();
 		/**
 		* @pre need to display the updated hangman figure
 		* @post updated hangman figure is generated
 		* @param none
 		* @return N/A
 		**/
+		void personDisplay();
 	
-		void isWinner();
 		/**
 		* @pre need to check if there is a winner and who it is
 		* @post winner is checked
 		* @param none
 		* @return N/A
 		**/
+		void isWinner();
 	
-		void reset();
 		/**
 		* @pre need to reset board and hangman figure
 		* @post board and hangman figure are reset
 		* @param none
 		* @return N/A
 		**/
+		void reset();
 };
 #endif
