60fc02938f5f2f61c2a77a59b37f2a83386ae49b
Zachary Misic <zachmisic@gmail.com>
2021-04-04 22:52:57 -0500

    Memory completed

--- main.cpp
+++ main.cpp
@@ -21,8 +21,8 @@ int main()
 		}
 		else if(option == 2)
 		{
-			blackjack myBlackJack;
-			myBlackJack.run();
+			Memory Mprogram;
+			Mprogram.run();
 		}
 		else if(option == 3)
 		{
@@ -31,6 +31,8 @@ int main()
 		}
 		else if(option == 4)
 		{
+			blackjack myBlackJack;
+			myBlackJack.run();
 		}
 		else if(option == 5)
 		{
