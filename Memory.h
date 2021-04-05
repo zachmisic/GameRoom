565974c0ef9bd884aca7ddc3216e1980af3fe656
Zachary Misic <zachmisic@gmail.com>
2021-04-04 21:41:13 -0500

    Created Memory game files and added "Memory" option to main.cpp

---
+++ Memory.h
@@ -0,0 +1,56 @@
+/*--------------------------------------------------------------
+ *
+ *@file: Memory.h
+ *@author: Zach Misic
+ *@assignment: EECS-448 project 3
+ *@description: Contains headers for the "Memory" class
+ *
+ --------------------------------------------------------------*/
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <iostream>
+#include <string.h>
+
+class Memory {
+
+private:
+
+	int size = 4;
+	char** board;
+	char cards[][] = { 'A','B','C','D','A','B','C','D','E','F','G','H','E','F','G','H' };
+
+
+public:
+
+	/**
+	* @pre N/A
+	* @post Creates an instance of the "Memory" class
+	* @return N/A
+	**/
+	Memory();
+
+	/**
+	* @pre An instance of "Memory" has been created
+	* @post Runs the memory game
+	* @return N/A
+	**/
+	void run();
+
+	/**
+	* @pre An instance of "Memory" has been created
+	* @post Populates the memory game board
+	* @return N/A
+	**/
+	void populate();
+
+	/**
+	* @pre An instance of "Memory" has been created and the board has been populated
+	* @post Displays the game board
+	* @return N/A
+	**/
+	void display();
+
+
+};
+#endif
