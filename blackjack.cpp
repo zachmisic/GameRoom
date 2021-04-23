d57553d4ffa6073273463fafaa859802ba5a5f2c
Jake Beesley <jakebees@gmail.com>
2021-04-23 18:09:14 -0500

    Created:   - superShuffle() method which shuffles the deck of cards thoroughly.   - initializehands() which populates the player and dealer's hands with empty cards   - printHands() used to test the cards in each hand   - dealPlayer() and dealHouse() which deals a card to each   - generateHouseValue() which returns the value of the dealer's hand   - playerAceChecker() and dealerAceChecker() which overrides an ace from 11 to 1 if the hand would bust   - endGame() which outputs the winner of the game based on the house and player values

--- blackjack.cpp
+++ blackjack.cpp
@@ -2,7 +2,7 @@
  *
  *@file: blackjack.cpp
  *@author: Jake Beesley
- *@assignment: EECS-448 project 3
+ *@assignment: EECS-448 project 4
  *@description: Contains the implementation for the blackjack program
  *
  --------------------------------------------------------------*/
@@ -10,49 +10,56 @@
 
 #include <iostream>
 #include "blackjack.h"
-#include "deck.h"
+
+//Runs the blackjack game
+void blackjack::run(){
+  blackjackGame();
+}
 
 //The full implementation of the blackjack game
 void blackjack::blackjackGame(){
-  int playerValue = 0;
-  int houseValue = 0;
+
   char gameContinuation;
   char userInput;
 
-  deck myDeck;
-  myDeck.shuffle();
-  myDeck.shuffle();
+  superShuffle();
+  initializeHands();
+
   std::cout << "\n\nWelcome to Blackjack!\n\n";
   std::cout << "House hits on a 16\n\n";
 
-  std::cout << "House's hand : | ** of ** | and ";
-  myDeck.printCard();
-  houseValue = myDeck.getValue();
-  std::cout << "House's current value : ?? + " << houseValue << "\n\n";
-  myDeck.deal(); //Adjusts to new card in the deck
+  dealerHand[0] = myDeck.nextCard();
+  playerHand[0] = myDeck.nextCard();
+  dealerHand[1] = myDeck.nextCard();
+  playerHand[1] = myDeck.nextCard();
+
+  std::cout << "House's hand : \t";
+  std::cout << dealerHand[0].print() << "\n";
+  std::cout << "and \t\t| ** of ** | \n\n";
+  std::cout << "House's current value : " << dealerHand[0].getValue() << " + ??\n\n";
 
   std::cout << "You have been dealt a: \t";
-  myDeck.printCard();
-  playerValue = myDeck.getValue();
-  myDeck.deal();
-  std::cout << "\t \t \t";
-  myDeck.printCard();
-  playerValue = myDeck.getValue() + playerValue;
-  myDeck.deal();
-  std::cout << "Value of the hand : " << playerValue << "\n\n";
+  std::cout << playerHand[0].print() << "\n";
+  std::cout << "\t\t\t" << playerHand[1].print() << "\n\n";
+
+  playerValue = playerHand[0].getValue() + playerHand[1].getValue();
+
+  if(playerValue == 22){
+    std::string sameSuit = playerHand[0].getSuit();
+    playerHand[0] = myDeck.dealSpecialAce(sameSuit);
+    playerValue = playerHand[0].getValue() + playerHand[1].getValue();
+  }
+
+  std::cout << "Your current value is : " << playerValue  << "\n\n";
 
   do{
     std::cout << "Do you want to hit or stay? (h/s): ";
     std::cin >> userInput;
-    if(userInput == 'h'){https://github.com/zachmisic/GameRoom.git
-      std::cout << "\n";
-      std::cout << "You have been dealt a: \t";
-      myDeck.printCard();
-      playerValue = myDeck.getValue() + playerValue;
-      myDeck.deal();
-      std::cout << "Value of the hand : " << playerValue << "\n";
+    if(userInput == 'h'){
+      dealPlayer();
     }
     else if(userInput == 's'){
+      system("clear");
       break;
     }
     else{
@@ -61,33 +68,32 @@ void blackjack::blackjackGame(){
   }while(playerValue < 21);
 
   if(playerValue > 21){
-    std::cout << "You busted! \n";
+    playerAceChecker();
+    std::cout << "You busted! \n\n";
   }
   else{
+    std::cout << "The house's first card was a : ";
+    std::cout << dealerHand[0].print() << "\n";
     std::cout << "The house's second card was a : ";
-    myDeck.printCard();
-    houseValue = myDeck.getValue() + houseValue;
-    std::cout << "House's current value : " << houseValue << "\n\n";
-    myDeck.deal();
+    std::cout << dealerHand[1].print() << "\n";
+    houseValue = dealerHand[0].getValue() + dealerHand[1].getValue();
+    std::cout << "House's current value : " << generateHouseValue() << "\n\n";
     if(houseValue < 17){
       do{
-        std::cout << "The house's has been dealt a : ";
-        myDeck.printCard();
-        houseValue = myDeck.getValue() + houseValue;
-        std::cout << "House's current value : " << houseValue << "\n\n";
-        myDeck.deal();
+        if(houseValue < 17){
+          dealHouse(); //deals to the house untill it busts or hits 17+ < 21
+        }
+        else{
+          break;
+        }
       }while(houseValue < 17);
     }
-    std::cout << "House's current value : " << houseValue << "\n";
-    std::cout << "Your current value : " << playerValue << "\n\n";
-    if(playerValue > houseValue || houseValue > 21){
-      std::cout << "You won! \n\n";
-    }
-    else if(houseValue > playerValue){
-      std::cout << "You lost! \n\n";
+    if(houseValue > 21){
+      dealerAceChecker();
+      endGame(); //Reveal both player's values, determine winner
     }
     else{
-      std::cout << "You both tied! \n\n";
+      endGame(); //Reveal both player's values, determine winner
     }
   }
 
@@ -105,8 +111,118 @@ void blackjack::blackjackGame(){
   }
 }
 
-//Runs the blackjack game
-void blackjack::run(){
-  blackjackGame();
+//Shuffles the deck 100 times
+void blackjack::superShuffle(){
+  for(int i = 0; i < 100; i++){
+    myDeck.shuffle();
+  }
 }
+
+//Sets each card in each hand to an empty card
+void blackjack::initializeHands(){
+  for(int i = 0; i < maximumCards; i++){
+    playerHand[i] = myDeck.dealEmptyCard();
+    dealerHand[i] = myDeck.dealEmptyCard();
+  }
+}
+
+//Tester function that outputs the information of each hand
+void blackjack::printHands(){
+  for(int i = 0; i < maximumCards; i++){
+    std::cout << "Player Card Number : " << i << " " << playerHand[i].print() << "\n";
+    std::cout << "House Card Number : " << i << " " << dealerHand[i].print() << "\n";
+  }
+}
+
+//Deals a card to the player
+void blackjack::dealPlayer(){
+  for(int i = 1; i <= maximumCards; i++){
+    if(playerHand[i+1] == myDeck.dealEmptyCard()){
+      playerHand[i+1] = myDeck.nextCard();
+      break;
+    }
+  }
+
+  std::cout << "\nYou have been dealt a: \t";
+
+  for(int j = 1; j < maximumCards; j++){
+    if(playerHand[j+1] != myDeck.dealEmptyCard()){
+      std::cout << playerHand[j+1].print() << "\n";
+      break;
+    }
+  }
+
+  for(int k = 2; k < maximumCards; k++){
+    playerValue = playerValue + playerHand[k].getValue();
+  }
+  playerAceChecker();
+  std::cout << "Value of your hand : " << playerValue << "\n\n";
+}
+
+//Deals a card to the house
+void blackjack::dealHouse(){
+  for(int i = 0; i < maximumCards; i++){
+    if(dealerHand[i] == myDeck.dealEmptyCard()){ //index 0 and index 1 should be auto skipped, due to dealerStart
+      dealerHand[i] = myDeck.nextCard();
+      break;
+    }
+  } 
+  std::cout << "\nHouse has been dealt a: \t";
+
+  for(int j = 1; j < maximumCards; j++){
+    if(dealerHand[j+1] == myDeck.dealEmptyCard()){
+      std::cout << dealerHand[j].print() << "\n";
+      break;
+    }
+  }
+  dealerAceChecker();
+  std::cout << "Value of house's hand : " << generateHouseValue() << "\n\n";
+}
+
+//Generates the value of the house's hand
+int blackjack::generateHouseValue(){
+  houseValue = 0;
+  for(int i = 0; i < maximumCards; i++){
+    houseValue = houseValue + dealerHand[i].getValue();
+  }
+  return(houseValue);
+}
+
+//Check if there is an ace in the player's hand
+void blackjack::playerAceChecker(){
+  if(houseValue < 10){
+    for(int i = 0; i < maximumCards; i++){
+      if(playerHand[i].getValue() == 11){
+        std::string sameSuit = playerHand[i].getSuit();
+        playerHand[i] = myDeck.dealSpecialAce(sameSuit);
+        break;
+      }
+    }
+  }
+}
+
+//Check if there is an ace in the dealer's hand
+void blackjack::dealerAceChecker(){
+  for(int i = 0; i < maximumCards; i++){
+    if(dealerHand[i].getValue() == 11){
+      std::string sameSuit = dealerHand[i].getSuit();
+      dealerHand[i] = myDeck.dealSpecialAce(sameSuit);
+      break;
+    }
+  }
+}
+
+//Determines the winner of the blackjack game
+void blackjack::endGame(){
+  std::cout << "House's current value : " << houseValue << "\n";
+  std::cout << "Your current value : " << playerValue << "\n\n";
+  if(playerValue > houseValue || houseValue > 21){
+    std::cout << "You won! \n\n";
+  }
+  else if(houseValue > playerValue){
+    std::cout << "You lost! \n\n";
+  }
+  else{
+    std::cout << "You both tied! \n\n";
+  }  
 }
--- blackjack.h
+++ blackjack.h
@@ -2,7 +2,7 @@
  *
  *@file: blackjack.h
  *@author: Jake Beesley
- *@assignment: EECS-448 project 3
+ *@assignment: EECS-448 project 4
  *@description: Header file for the blackjack program
  *
  --------------------------------------------------------------*/
@@ -10,6 +10,8 @@
 #ifndef BLACKJACK_H
 #define BLACKJACK_H
 #include <string>
+#include <stdlib.h>
+#include "deck.h"
 
 class blackjack
 {
@@ -29,5 +31,76 @@ public:
   * @return N/A
   **/
   void blackjackGame();
+
+  /**
+  * @pre blackjackGame() is called
+  * @post Shuffles the deck of cards 100 times
+  * @return N/A
+  **/
+  void superShuffle();
+
+  /**
+  * @pre blackjackGame() is called
+  * @post Fills the dealer and player's hands with empty cards
+  * @return N/A
+  **/
+  void initializeHands();
+
+  /**
+  * @pre N/A
+  * @post Outputs every card of the dealer and player's hands
+  * @return N/A
+  **/
+  void printHands();
+
+  /**
+  * @pre Player chooses to hit within blackjackGame()
+  * @post Checks if the next index of the player's hand is empty, then replaces it with a card from the deck
+  * @return N/A
+  **/
+  void dealPlayer();
+
+  /**
+  * @pre houseValue is less than 17 within blackjackGame()
+  * @post Checks if the next index of the dealer's hand is empty, then replaces it with a card from the deck
+  * @return N/A
+  **/
+  void dealHouse();
+
+  /**
+  * @pre N/A
+  * @post Sums the values of the house's hand
+  * @return integer: houseValue (the value of the house's hand)
+  **/
+  int generateHouseValue();
+
+  /**
+  * @pre The player is going to bust due to an ace in their hand
+  * @post Replaces the value of the ace from an 11 to a 1
+  * @return N/A
+  **/
+  void playerAceChecker();
+
+  /**
+  * @pre The dealer is going to bust due to an ace in their hand
+  * @post Replaces the value of the ace from an 11 to a 1
+  * @return N/A
+  **/
+  void dealerAceChecker();
+
+  /**
+  * @pre N/A
+  * @post Outputs the values of the player and dealer's hand in order to determine a winner
+  * @return N/A
+  **/
+  void endGame();
+
+  deck myDeck;
+  int playerValue = 0;
+  int houseValue = 0;
+  int maximumCards = 10;
+  card dealerHand[10];
+  card playerHand[10];
+
 };
 #endif
